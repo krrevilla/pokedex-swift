75e0b352dc53da834f4560d02066cf5a8a9cc795
Karl Revilla <karl.revilla@bilue.com.au>
2023-08-21 10:04:13 +0800

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Karl Revilla on 21/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
